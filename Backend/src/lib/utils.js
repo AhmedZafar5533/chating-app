f165e27abd292d22e7b97058f9b64ced9fbeef84
Ahmed Zafar <153266724+AhmedZafar5533@users.noreply.github.com>
2024-12-29 19:01:37 +0500

    Update utils.js

--- Backend/src/lib/utils.js
+++ Backend/src/lib/utils.js
@@ -8,7 +8,7 @@ const generateToken = (userId, res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: "strict",
-        secure: false,
+        secure: true,
     });
 };
 module.exports = generateToken;
